7bb3a966fe4a49edd5b924c30b2afb0c947e259d
❌Chen <93198837+Chenishere@users.noreply.github.com>
2021-11-22 23:49:31 +0100

    Create yt.sh

---
+++ TP3/etc/systemd/system/yt.sh
@@ -0,0 +1,16 @@
+downloads="/srv/yt/downloads"
+logs="/var/log/yt"
+if [[ (-d "$dl") && (-d "$log")]]; then
+	url=$1
+	title=$(youtube-dl -j $url | cut -d'"' -f8)
+	way="/srv/yt/downloads/$title"
+	mkdir "$way"
+	mkdir "$way/description"
+	echo "Video $url was downloaded."
+        echo "File path : $way/$title.mp4"
+	youtube-dl -o "$way/%(title)s.%(ext)s" "$url" > "/dev/null"
+	youtube-dl -o "$way/description/$title" --skip-download --youtube-skip-dash-manifest "https://www.youtube.com/watch?v=G_1IyYRdLBA" > "/dev/null"
+	echo "[$(date "+%Y/%m/%d %T")] Video $url was downloaded. File path : $way/$title.mp4" >> "$log/download.log"
+else
+	exit
+fi
